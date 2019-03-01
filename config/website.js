2bfb472c3e1b9ae8c89909aba41879905d4426f2
Emmanuel ERNEST <ernest.emmanuel@hotmail.fr>
2019-03-01 11:37:59 +0100

    feat(theme): modify theme to captaindev

--- config/website.js
+++ config/website.js
@@ -3,23 +3,23 @@ const tailwind = require('../tailwind')
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: 'Cara - Gatsby Starter Portfolio', // Navigation and Site Title
-  siteTitleAlt: 'Cara', // Alternative Site title for SEO
-  siteTitleShort: 'Cara', // short_name for manifest
-  siteHeadline: 'Creating marvelous art & blazginly fast websites', // Headline for schema.org JSONLD
-  siteUrl: 'https://cara.lekoarts.de', // Domain of your site. No trailing slash!
-  siteLanguage: 'en', // Language Tag on <html> element
+  siteTitle: 'Captain Dev - Deviens un leader technique', // Navigation and Site Title
+  siteTitleAlt: 'Captain Dev', // Alternative Site title for SEO
+  siteTitleShort: 'Captain Dev', // short_name for manifest
+  siteHeadline: "Captain Dev vous accompagne dans l'amélioration des performances de votre site web", // Headline for schema.org JSONLD
+  siteUrl: 'https://www.captaindev.io', // Domain of your site. No trailing slash!
+  siteLanguage: 'fr', // Language Tag on <html> element
   siteLogo: '/logo.png', // Used for SEO and manifest
   siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
-  author: 'LekoArts', // Author for schema.org JSONLD
+  author: 'Emmanuel ERNEST', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@cara', // Twitter Username
-  ogSiteName: 'cara', // Facebook Site Name
-  ogLanguage: 'en_US', // Facebook Language
-  googleAnalyticsID: 'UA-47519312-5',
+  userTwitter: '@captaindev404', // Twitter Username
+  ogSiteName: 'Captain-Dev-415521452355443', // Facebook Site Name
+  ogLanguage: 'fr_FR', // Facebook Language
+  googleAnalyticsID: 'UA-120679952-1',
 
   // Manifest and Progress color
-  themeColor: tailwind.colors.orange,
+  themeColor: tailwind.colors['purple-dark'],
   backgroundColor: tailwind.colors.blue,
 }
